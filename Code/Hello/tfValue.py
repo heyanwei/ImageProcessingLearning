9be6a6ec078762464be2419acdbdafa92b704776
HeYanwei <heyw@yeefung.com>
2019-12-16 22:11:26 +0800

    变量的数据类型初始化需要调整初始化器的位置

--- Code/Hello/tfValue.py
+++ Code/Hello/tfValue.py
@@ -3,16 +3,22 @@ import tensorflow as tf
 
 g = tf.Graph()
 with g.as_default():
-    sess = tf.compat.v1.Session(graph=g)
-    init_op = tf.compat.v1.global_variables_initializer()
-
     data1 = tf.constant(2.5) # 常量
-    data1_1 = tf.constant(2, dtype=tf.int32)
-
+    data1_1 = tf.constant(2, dtype=tf.int32) 
     data2 = tf.Variable(10,name='var') # 变量
-    # 变量需要初始化   
-    sess.run(init_op)
+
+    init_op = tf.compat.v1.global_variables_initializer()
+    sess = tf.compat.v1.Session(graph=g)
+    with sess:
+        # 变量需要初始化   
+        sess.run(init_op)
     
-    print(sess.run(data1))
-    print(sess.run(data1_1))
-    # print(sess.run(data2))
+        print(sess.run(data1))
+        print(sess.run(data1_1))
+        print(sess.run(data2))
+
+        # 本质 tf = tensor+ 计算图
+        # tensor 数据
+        # op
+        # graphs 数据操作
+        # session
